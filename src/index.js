c6f73bf13c4c435569d8de12b135d288cd8ef7f5
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-17 08:49:28 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-const init = () => {
-  
-}
+ const init = () => {
+     const inputForm = document.querySelector("form")
+     inputForm.addEventListener("submit", (e)=> {
+         e.preventDefault();
+ 
+     const input = document.querySelector('input#searchByID');
 
-document.addEventListener('DOMContentLoaded', init);
+     fetch(`http://localhost:3000/movies/${input.value}`)
+     .then((response1) => response1.json())
+     .then(data => {
+         const title = document.querySelector("section#movieDetails h4");
+         const summary = document.querySelector("section#movieDetails p");
+         title.innerHTML = data.title;
+         summary.innerHTML = data.summary;
+
+     }); 
+    });
+}
+ document.addEventListener('DOMContentLoaded', init);
+
+
+
+  
